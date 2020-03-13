3d535d99f783e03a3a1c5398509300fe01973eaf
OldProject1 <47574197+OldProject1@users.noreply.github.com>
2020-03-13 13:57:38 +0300

    Update lab2.cs

--- lab2.cs
+++ lab2.cs
@@ -1,47 +1,23 @@
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System;
-using static SandBox.SP;
 
 namespace SandBox
 {
-    class SP {
+    class MyClass
+    {
         public static string GetBinaryStr(int num, int length)
         {
-            return Convert.ToString(num, 2).PadLeft(length,'0');
-        }
-        public static string GetStrByPos (string baseStr, int[] pos)
-        {
-            string result = "";
-            foreach (int position in pos)
-            {
-                result += baseStr[position - 1];
-            }
-            return result;
+            return Convert.ToString(num, 2).PadLeft(length, '0');
         }
         public static string GetSquareSum(string first, string second, int length)
         {
             int result = Convert.ToInt32(first, 2) + Convert.ToInt32(second, 2);
             return GetBinaryStr(result % (int)Math.Pow(2, length), length);
         }
-        public static void SplitIntoBlocks(string source, out string T1, out string T2)
-        {
-            T1 = source.Substring(0, source.Length / 2);
-            T2 = source.Substring(source.Length / 2, source.Length / 2);
-        }
-        public static string P(string source)
-        {
-            string prt1, prt2;
-            int step = 6;
-            prt1 = source.Substring(0, step);
-            prt2 = source.Substring(step, source.Length - step);
-            return prt2 + prt1;
-        }
         public static string E(string X, string k)
         {
             string k_1, k_2, k_3;
+            var GetStrByPos = (Func<string, int[], string>) ((str, arr) => new string(arr.Select(num => str[num - 1]).ToArray()));
             k_1 = GetStrByPos(k, new int[] { 10, 12, 2, 5, 8, 6, 9, 4 });
             k_2 = GetStrByPos(k, new int[] { 2, 9, 10, 5, 1, 12, 6, 4 });
             k_3 = GetStrByPos(k, new int[] { 7, 1, 2, 6, 12, 3, 9, 11 });
@@ -50,32 +26,31 @@ namespace SandBox
             string[] S2 = "E 7 A C D 1 3 9 0 2 B 4 F 8 5 6".Split(' ');
             S1 = (from item in S1 select GetBinaryStr(Convert.ToInt32(item, 16), 4)).ToArray();
             S2 = (from item in S2 select GetBinaryStr(Convert.ToInt32(item, 16), 4)).ToArray();
+            var P = (Func<string, int, string>)((data, step) => data.Substring(0, step) + data.Substring(step, data.Length - step));
             for (int i = 0; i < 3; i++)
             {
                 X = GetSquareSum(X, roundKeys[i], X.Length);
                 string T1, T2;
-                SplitIntoBlocks(X, out T1, out T2);
+                T1 = X.Substring(0, X.Length / 2);
+                T2 = X.Substring(X.Length / 2, X.Length / 2);
                 string N1, N2;
                 N1 = S1[Convert.ToInt32(T1, 2)];
                 N2 = S2[Convert.ToInt32(T2, 2)];
                 X = N1 + N2;
-                X = P(X);
+                X = P(X, 6);
                 Console.WriteLine("X = " + X);
             }
             return X;
         }
-    }
-    class MyClass {
         static void Main(string[] args)
         {
             try
             {
                 int N = 14;
                 int q = 4, r = 9;
-                string X = GetBinaryStr(7 * N, 8), 
-                    key = GetBinaryStr(4096 - 11 * q * r, 12);
+                string X = GetBinaryStr(7 * N, 8), key = GetBinaryStr(4096 - 11 * q * r, 12);
                 Console.WriteLine("X = " + X);
-                Console.WriteLine(key);
+                Console.WriteLine("key = " + key);
                 E(X, key);
                 Console.WriteLine();
                 E("01100000", key);
