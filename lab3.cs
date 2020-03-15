64584ba7e65267c8d32c83d1ef9e7ba4bbbebff8
OldProject1 <47574197+OldProject1@users.noreply.github.com>
2020-03-15 23:22:03 +0300

    Update lab3.cs

--- lab3.cs
+++ lab3.cs
@@ -10,21 +10,10 @@ namespace SandBox
 {
     class LFSR
     {
-        public string condition;
-        public string s;
+        
         public string C { get; private set; }
-        public string S
-        {
-            get
-            {
-                return s;
-            }
-            set
-            {
-                condition = value;
-                s = new string(value.Reverse().ToArray());
-            }
-        }
+        public string S { get; private set; }
+    
         public LFSR (string c, string s)
         {
             C = c;
@@ -32,23 +21,22 @@ namespace SandBox
         }
         public string GetNext()
         {
-            string output = S[0] + "";
-            string next_S_rev = "";
+            int L = S.Length;
+            string output = S[L-1] + "";
+            string next_S = "";
 
             int CharToInt(char ch) => ch - '0';
             bool CharToBool(char ch) => ch == '1';
             bool IntToBool(int i) => i == 1;
 
-            for (int i = 0; i <= S.Length - 2; i++)
+            
+            next_S += CharToInt(S[L -1]) * CharToInt(C[L -1]);
+            for (int i = L - 2; i >= 0; i--)
             {
-                next_S_rev += Convert.ToInt32(IntToBool(CharToInt(S[0]) * CharToInt(C[i])) ^ CharToBool(S[i + 1]));
+                next_S += Convert.ToInt32(IntToBool(CharToInt(S[L - 1]) * CharToInt(C[i])) ^ CharToBool(S[L - i - 2]));
             }
-            next_S_rev += CharToInt(S[0]) * CharToInt(C[C.Length - 1]);
-
-
-            S = new string(next_S_rev.Reverse().ToArray());
-
-
+            S = next_S;
+            
             return output;
         }
     }
@@ -60,28 +48,30 @@ namespace SandBox
             
             try
             {
-                Stopwatch sw = new Stopwatch();
-                sw.Start();
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
 
                 string HexToBin(string hexStr) => Convert.ToString(Convert.ToInt32(hexStr, 16), 2);
-                LFSR one = new LFSR(HexToBin("0x400062"), HexToBin("0x6DB8F9"));
-                LFSR two = new LFSR(HexToBin("0x100000DF"), HexToBin("0x167EE042"));
-                LFSR three = new LFSR(HexToBin("0x40000097"), HexToBin("0x55B7BC02"));
+                //LFSR one = new LFSR(HexToBin("0x400062"), HexToBin("0x6DB8F9"));
+                //LFSR two = new LFSR(HexToBin("0x100000DF"), HexToBin("0x167EE042"));
+                //LFSR three = new LFSR(HexToBin("0x40000097"), HexToBin("0x55B7BC02"));
 
-                int CharToInt(char ch) => ch - '0';
-                bool CharToBool(char ch) => ch == '1';
+                LFSR one = new LFSR(HexToBin("0x400051"), HexToBin("0x6980D6"));
+                LFSR two = new LFSR(HexToBin("0x100000C8"), HexToBin("0x1016FD2E"));
+                LFSR three = new LFSR(HexToBin("0x4000007F"), HexToBin("0x732DB497"));
+
                 bool StrToBool(string str) => Convert.ToInt32(str) == 1;
                 bool IntToBool(int i) => i == 1;
+                int n = 32; //1250000
 
-                int n = 1250000; //1250000
-                //string part2() => ()
-                string getGamma() {
+                string getGamma()
+                {
                     string one_next = one.GetNext();
                     string two_next = two.GetNext();
                     string three_next = three.GetNext();
                     bool part1() => IntToBool(Convert.ToInt32(one_next) * Convert.ToInt32(two_next));
                     bool part2() => IntToBool(Convert.ToInt32(StrToBool(one_next) ^ true) * Convert.ToInt32(three_next));
-                    return Convert.ToInt32(part1()^part2()).ToString();
+                    return Convert.ToInt32(part1() ^ part2()).ToString();
                 }
 
                 string gammaSeq = "";
@@ -89,18 +79,22 @@ namespace SandBox
                 {
                     gammaSeq += getGamma();
                 }
-
                 string path = @"C:\Users\user\Desktop\result.txt";
                 File.WriteAllText(path, string.Empty);//but you can use: File.Delete(path);
-                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
+                //File.WriteAllText(path, gammaSeq);
+                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
                 {
-                    writer.Write(gammaSeq.Select(item => Convert.ToByte(item)).ToArray());
+                    writer.Write(10);
+                    writer.Write(25);
+                    writer.Write(31);
+                    writer.Write(5235);
+                    writer.Write(2);
+                    writer.Write(512);
+
                 }
 
-                
-                
-                sw.Stop();
-                Console.WriteLine(((sw.ElapsedMilliseconds / 100.0).ToString()));
+                timer.Stop();
+                Console.WriteLine((timer.ElapsedMilliseconds / 100.0).ToString());
             }
             catch (Exception ex)
             {
